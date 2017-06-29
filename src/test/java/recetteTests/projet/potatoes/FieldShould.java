d98972f5b51a5d69ea2884dcea932990deba8de1
L. Hebert <lucyhebert@free.fr>
2017-06-29 19:23:02 +0200

    Add test on Field

--- src/test/java/recetteTests/projet/potatoes/FieldShould.java
+++ src/test/java/recetteTests/projet/potatoes/FieldShould.java
@@ -1,5 +1,25 @@
 package recetteTests.projet.potatoes;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
 public class FieldShould {
+	
+	Game game;
+	Field field;
+	
+	@Before
+	public void setup() {
+		Field.setSeed(5);
+		this.game = new Game(); 
+		this.field = new Field(game);
+	}
+	
+	@Test
+	public void shouldHavePlots() {
+		assertEquals(true, field.getPlots().length > 0);
+	}
 
 }
