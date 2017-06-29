c53b7db3b82c17992656feddb573148b83498f29
L. Hebert <lucyhebert@free.fr>
2017-06-29 22:06:36 +0200

    Add new tests on Field

--- src/test/java/recetteTests/projet/potatoes/FieldShould.java
+++ src/test/java/recetteTests/projet/potatoes/FieldShould.java
@@ -2,6 +2,10 @@ package recetteTests.projet.potatoes;
 
 import static org.junit.Assert.assertEquals;
 
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -9,17 +13,42 @@ public class FieldShould {
 	
 	Game game;
 	Field field;
+	int mouseX = 84;
+	int mouseY = 82;
 	
 	@Before
 	public void setup() {
 		Field.setSeed(5);
-		this.game = new Game(); 
-		this.field = new Field(game);
+		game = new Game(); 
+		field = new Field(game);
 	}
 	
 	@Test
-	public void shouldHavePlots() {
+	public void havePlots() {
 		assertEquals(true, field.getPlots().length > 0);
+	}
+	
+	@Test
+	public void haveOneContaminatedPotatoOnCreate() {
+		assertEquals(true, field.unhealthyPlots.size() == 1);
+	}
+	
+	@Test
+	public void killPlayerIfDigsOnContaminatingPotato() {
+		// Prepare to capture output
+		PrintStream originalOut = System.out;
+		OutputStream os = new ByteArrayOutputStream();
+		PrintStream ps = new PrintStream(os);
+		System.setOut(ps);
+		String separator = System.getProperty("line.separator");
+		
+		// Perform test
+		field.getSelectedPlot(mouseX, mouseY).getPotato().setState(3);
+		field.digPlot(mouseX, mouseY);
+		assertEquals("You're DEAD motherfucker !" + separator, os.toString());
+		
+		System.setOut(originalOut);
+		
 	}
 
 }
